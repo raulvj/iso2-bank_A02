f0877cf39900659ae11941569fb5f83b0f97668a
raulvj <raulvj@ZenithAce>
2019-12-23 13:35:23 +0100

    Testing TarjetaDebito

--- banco20193capas/src/test/java/edu/uclm/esi/iso2/banco20193capas/TestTarjetaDebito.java
+++ banco20193capas/src/test/java/edu/uclm/esi/iso2/banco20193capas/TestTarjetaDebito.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import edu.uclm.esi.iso2.banco20193capas.exceptions.PinInvalidoException;
+import edu.uclm.esi.iso2.banco20193capas.exceptions.TarjetaBloqueadaException;
 import edu.uclm.esi.iso2.banco20193capas.model.Cliente;
 import edu.uclm.esi.iso2.banco20193capas.model.Cuenta;
 import edu.uclm.esi.iso2.banco20193capas.model.Manager;
+import edu.uclm.esi.iso2.banco20193capas.model.TarjetaCredito;
 import edu.uclm.esi.iso2.banco20193capas.model.TarjetaDebito;
 import junit.framework.TestCase;
 
@@ -45,6 +47,77 @@ public class TestTarjetaDebito extends TestCase {
 			
 		}catch (Exception e) {
 			fail("Esperaba PinInvalidoException" + e);
+		}
+	}
+	
+	@Test
+	public void testCompraPorInternetConTD() {
+		try {
+			Cliente pepe = new Cliente("12345X", "Pepe", "Pérez");
+			pepe.insert();
+		
+			Cuenta cuentaPepe = new Cuenta(1);
+			cuentaPepe.addTitular(pepe);
+			cuentaPepe.insert();
+			
+			cuentaPepe.ingresar(1000);
+			cuentaPepe.retirar(200);;
+			assertTrue(cuentaPepe.getSaldo()==800);
+			
+			TarjetaDebito td = cuentaPepe.emitirTarjetaDebito("12345X");
+			int token = td.comprarPorInternet(td.getPin(), 300);
+			td.confirmarCompraPorInternet(token);
+		} catch (Exception e) {
+			fail("Excepción inesperada: " + e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testTarjetaDebitoBloqueada() {
+		try {
+			Cliente pepe = new Cliente("12345X", "Pepe", "Pérez");
+			pepe.insert();
+			
+			Cuenta cuentaPepe = new Cuenta(1);
+			cuentaPepe.addTitular(pepe);
+			cuentaPepe.insert();
+		
+			TarjetaDebito tdPepe = cuentaPepe.emitirTarjetaDebito(pepe.getNif());
+
+			for (int i = 0; i<3; i++) {
+				try {
+					tdPepe.sacarDinero(tdPepe.getPin()+1, 100);
+					fail("Esperaba PinInvalidoException");
+				} catch (PinInvalidoException e) {
+				} catch (Exception e) {
+					fail("Esperaba PinInvalidoException y recibio: " + e);
+				}
+			}
+			assertTrue(tdPepe.isActiva() == false);
+			tdPepe.sacarDinero(tdPepe.getPin(), 100);
+			fail("Esperaba TarjetaBloqueadaException");
+		} catch (TarjetaBloqueadaException e) {
+		} catch (Exception e) {
+			fail("Unexpected Exception " + e);
+		}
+	}
+	
+	@Test
+	public void testTarjetaDebitoSacarDinero() {
+		try {
+			Cliente pepe = new Cliente("12345X", "Pepe", "Pérez");
+			pepe.insert();
+			
+			Cuenta cuentaPepe = new Cuenta(1);
+			cuentaPepe.addTitular(pepe);
+			cuentaPepe.insert();
+			
+			cuentaPepe.ingresar(1000);
+
+			TarjetaDebito tdPepe = cuentaPepe.emitirTarjetaDebito("12345X");
+			tdPepe.sacarDinero(tdPepe.getPin(), 100);
+		} catch (Exception e) {
+			fail("Unexpected Exception " + e);
 		}
 	}
 }
